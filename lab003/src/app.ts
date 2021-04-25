58f608cef0bbffecff445a68abff7beb23f27a66
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-04-25 11:06:59 +0200

    Valid data from api

--- lab003/src/app.ts
+++ lab003/src/app.ts
@@ -1,28 +1,36 @@
 export class App {
-    opwApiKey = '50d53005c0fd5f556bb4ef15224c4209';
-    constructor() {
-        this.getCityInfo('zakopane')
+  opwApiKey = "2eab94e68e577bc023a653cc6be04573";
+  constructor() {
+    this.getCityInfo("krakow");
+  }
+  async getCityInfo(city: string) {
+    const weather = await this.getWeather("krakow");
+    this.saveData(weather);
+    this.showData();
+  }
+  async getWeather(city: string): Promise<any> {
+    const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
+    const weatherResponse = await fetch(openWeatherUrl);
+    const weatherData = await weatherResponse.json();
+    console.log(weatherData);
+    return weatherData;
+  }
+  saveData(data: any) {
+    localStorage.setItem("weatherData", JSON.stringify(data));
+  }
+  getData() {
+    const data = localStorage.getItem("weatherData");
+    if (data) {
+      return JSON.parse(data);
+    } else {
+      return {};
     }
-    async getCityInfo(city: string) {
-        const weather = await this.getWeather('zakopane');
-        this.saveData(weather);
-    }
-    async getWeather(city: string): Promise<any> {
-        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
-        const weatherResponse = await fetch(openWeatherUrl);
-        const weatherData = await weatherResponse.json();
-        console.log(weatherData);
-        return weatherData;
-    }
-    saveData(data: any) {
-        localStorage.setItem('weatherData', JSON.stringify(data));
-    }
-    getData() {
-        const data = localStorage.getItem('weatherData');
-        if (data) {
-            return JSON.parse(data);
-        } else {
-            return {};
-        }
-    }
+  }
+  showData() {
+    const data = this.getData();
+    const temperature = (data.main.temp - 273.15).toFixed();
+    const pressure = data.main.pressure;
+    const humidity = data.main.humidity;
+    console.log(temperature, pressure, humidity);
+  }
 }
