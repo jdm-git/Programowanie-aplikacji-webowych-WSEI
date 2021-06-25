7ff742a92041e33ee9145fb94790c6eed794978d
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-06-25 15:09:55 +0200

    recording 2nd channel

--- lab002/index.js
+++ lab002/index.js
@@ -8,17 +8,36 @@ var snare;
 var tink;
 var tom;
 var channel1 = [];
+var channel2 = [];
+var selectedChannel = channel1;
 appStart();
 function appStart() {
     document.body.addEventListener("keypress", onKeyDown);
     var btnChannel1Play = document.querySelector("#btnChannel1");
+    var btnChannel2Play = document.querySelector("#btnChannel2");
+    var btnRecord1 = document.querySelector("#btnRecord1");
+    var btnRecord2 = document.querySelector("#btnRecord2");
+    btnRecord1.addEventListener("click", setRecording1);
+    btnRecord2.addEventListener("click", setRecording2);
     btnChannel1Play.addEventListener("click", onPlayChanell1);
+    btnChannel2Play.addEventListener("click", onPlayChanell2);
     getSounds();
 }
 function onPlayChanell1() {
     channel1.forEach(function (sound) {
         setTimeout(function () { return playSound(sound.key); }, sound.time);
     });
+}
+function onPlayChanell2() {
+    channel2.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.time);
+    });
+}
+function setRecording1() {
+    selectedChannel = channel1;
+}
+function setRecording2() {
+    selectedChannel = channel2;
 }
 function getSounds() {
     hihatSound = document.querySelector('[data-sound="hihat"]');
@@ -35,7 +54,10 @@ function onKeyDown(ev) {
     console.log(ev);
     var key = ev.key;
     var time = ev.timeStamp;
-    channel1.push({ key: key, time: time }); // skrocona notacja key: value
+    if (selectedChannel == channel1)
+        channel1.push({ key: key, time: time }); // skrocona notacja key: value
+    if (selectedChannel == channel2)
+        channel2.push({ key: key, time: time });
     playSound(key);
     var focusButton = function () {
         var btn = document.getElementById(key);
--- lab002/index.ts
+++ lab002/index.ts
@@ -10,20 +10,36 @@ let tom: HTMLAudioElement;
 
 const channel1: any[] = [];
 const channel2: any[] = [];
-const channel3: any[] = [];
-const channel4: any[] = [];
+let selectedChannel: any = channel1;
 
 appStart();
 function appStart(): void {
   document.body.addEventListener("keypress", onKeyDown);
   const btnChannel1Play = document.querySelector("#btnChannel1");
+  const btnChannel2Play = document.querySelector("#btnChannel2");
+  const btnRecord1 = document.querySelector("#btnRecord1");
+  const btnRecord2 = document.querySelector("#btnRecord2");
+  btnRecord1.addEventListener("click", setRecording1);
+  btnRecord2.addEventListener("click", setRecording2);
   btnChannel1Play.addEventListener("click", onPlayChanell1);
+  btnChannel2Play.addEventListener("click", onPlayChanell2);
   getSounds();
 }
 function onPlayChanell1(): void {
   channel1.forEach((sound) => {
     setTimeout(() => playSound(sound.key), sound.time);
   });
+}
+function onPlayChanell2(): void {
+  channel2.forEach((sound) => {
+    setTimeout(() => playSound(sound.key), sound.time);
+  });
+}
+function setRecording1(): void {
+  selectedChannel = channel1;
+}
+function setRecording2(): void {
+  selectedChannel = channel2;
 }
 function getSounds(): void {
   hihatSound = document.querySelector('[data-sound="hihat"]');
@@ -41,8 +57,8 @@ function onKeyDown(ev: KeyboardEvent): void {
   console.log(ev);
   const key = ev.key;
   const time = ev.timeStamp;
-  channel1.push({ key, time }); // skrocona notacja key: value
-
+  if (selectedChannel == channel1) channel1.push({ key, time }); // skrocona notacja key: value
+  if (selectedChannel == channel2) channel2.push({ key, time });
   playSound(key);
   const focusButton = () => {
     let btn = document.getElementById(key);
