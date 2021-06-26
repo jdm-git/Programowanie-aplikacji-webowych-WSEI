b1cdb59ce229d6bad01cfb80adc1a9bc5a6b0b73
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-06-26 20:14:06 +0200

    note class + utils

--- lab004/src/note.ts
+++ lab004/src/note.ts
@@ -1,11 +1,92 @@
-export class Note {
-  title: string;
-  content: string;
-  isPinned: boolean;
+import { dateToRelativeString, htmlToElement } from "./Utils";
 
-  constructor(title: string, content: string, isPinned = false) {
+export class Note {
+  id: string | null;
+  title: string;
+  message: string;
+  color: string;
+  pinned: boolean;
+  createdAt: Date;
+  editedAt?: Date;
+
+  constructor(
+    title: string,
+    message: string,
+    color: string = "#fff",
+    pinned: boolean = false,
+    createdAt: Date = new Date(),
+    editedAt: Date = null,
+    id: string | null = null
+  ) {
     this.title = title;
-    this.content = content;
-    this.isPinned = isPinned;
+    this.message = message;
+    this.color = color;
+    this.pinned = pinned;
+    this.createdAt = createdAt;
+    this.editedAt = editedAt;
+    this.id = id;
   }
+  togglePinned(): void {
+    this.pinned = !this.pinned;
+  }
+  createNote(): Element {
+    return htmlToElement(`
+            <div class="note" style="--background: ${this.color};">
+                <div class="header">
+                    <h2 class="edit-hidden">${this.title}</h2>
+                    <input type="text" class="edit-visible" value="${
+                      this.title
+                    }" name="title" />
+                    <span class="pin edit-hidden"><i class="${
+                      this.pinned ? "fas" : "far"
+                    } fa-bookmark"></i></span>
+                </div>
+                <div class="details">
+                    <span class="edit-hidden">${this.message}</span>
+                    <textarea class="edit-visible" name="message">${
+                      this.message
+                    }</textarea>
+                    <div class="bottom">
+                        <div class="actions edit-hidden">
+                            <span class="remove" title="Usuń">
+                                <i class="fas fa-trash"></i>
+                            </span>
+                            <span class="edit" title="Edytuj">
+                                <i class="far fa-edit"></i>
+                            </span>
+                            <span class="change-color" title="Zmień kolor">
+                                <i class="fas fa-palette"></i>
+                            </span>
+                        </div>
+                        <div class="save edit-visible">
+                            <button class="edit-save">Zapisz</button>
+                        </div>
+                        <div class="created-at">
+                            ${dateToRelativeString(this.createdAt)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+  }
+  static createFromJson(data: NoteInterface): Note {
+    return new Note(
+      data.title,
+      data.message,
+      data.color,
+      data.pinned,
+      new Date(data.createdAt),
+      data.editedAt !== null ? new Date(data.editedAt) : null,
+      data.id
+    );
+  }
+}
+interface NoteInterface {
+  id: string;
+  title: string;
+  message: string;
+  color: string;
+  pinned: boolean;
+  createdAt: string;
+  editedAt?: string;
 }
